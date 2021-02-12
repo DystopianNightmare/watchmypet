b486d95d887a3213af9b6da46a62daf2308d65e0
DystopianNightmare <52088130+DystopianNightmare@users.noreply.github.com>
2021-02-11 20:08:08 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<link href="style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+
+
+<div class = "body">
+<div class="mainleft">left</div>
+<div class="mainright">right</div>
+<div class ="top"> header</div>
+
+
+<div class ="nav">
+	<ul>
+		<li><a href="index.html">Home</a></li>
+		<li><a href="mypet.html">My Pet</a></li>
+		<li><a href="invite.html">Invite Someone</a></li>
+		<div class="login">
+			<div class="logintext">Login to see your pet!</div>
+			<form method="post" action="login.php">
+				<input type="text" placeholder="Email" name="email">
+				<input type="text" placeholder="Password" name="password">
+				<button type="submit">Login</button>
+				
+		</div>
+	</ul>
+</div>
+
+<div class="middlesection">
+<div class ="column left">WHITESPACE </div>
+<div class ="column middle">
+<p id="wondertext">Ever Wonder what you pet does at home when you are not around? Well now you can check up on them from anywhere.</p>
+</div>
+<div class ="column right"> <p id="meetmycat">Meet My Cat</p>
+<iframe width="70%"  height="70%" src="https://www.youtube.com/embed/Tp2CWeyvju8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture "  	allowfullscreen></iframe>
+</div>
+</div>
+</div>
+
+<div class ="bottom"> s</div>
+
+
+
+</body>
+</html>
+
+
+
